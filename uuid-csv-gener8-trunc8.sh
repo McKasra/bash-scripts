e55cab04ce050739c4ce7619f2bc13ba16926d50
McKasra <50338667+McKasra@users.noreply.github.com>
2019-06-12 10:41:37 -0700

    Update uuid-csv-gener8-trunc8.sh

--- uuid-csv-gener8-trunc8.sh
+++ uuid-csv-gener8-trunc8.sh
@@ -1,19 +1,19 @@
 #!/usr/local/bin/bash
 
-# Run the "uuidgen -hdr" n=3 times and save the results to a CSV
+# Run the "uuidgen -hdr" n times and save the results to a CSV
 echo Enter how many UUIDs you want using 1..n range format:
 read range1
 echo Output CSV filename without an extension or spaces:
 read pathToCSV
 for run in $(eval echo {$range1}); do uuidgen -hdr; done > $pathToCSV".csv"
 
-# Remove extraneous lines that uuidgen generates by default
+# Remove extraneous lines that uuidgen writes to file by default
 for i in warning, define
 do
   sed -i '' '/i/d' $pathToCSV".csv"
 done
 
-# Truncate the first three non-alphanumeric characters that uuidgen generates by default
+# Truncate the first three non-alphanumeric characters that uuidgen writes to file by default
 sed -i '' 's:^\/\/ ::' $pathToCSV".csv"
 
 # Truncate UUID to your desired number of characters, or leave as is by entering 1-36
