0e646c97738528cb171c51b861d5bcbea86ff8f4
McKasra <50338667+McKasra@users.noreply.github.com>
2021-03-22 11:30:29 -0700

    Add files via upload
    Two Bash scripts that generate a SQL INSERT STATEMENT for multiple rows from CSV data and outputs it to a text file (which can then be copied and pasted in SQL query box, e.g. on MySQL): <sql-insert.sh> auto-quotes string values and lists out column names in the SQL INSERT STATEMENT, but <sql-insert-wocol.sh> uses an alternative syntax without having to list out column names.

---
+++ sql-insert-wocol.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Prompt for input and: 1. enter CSV path to be imported into DB; 2. its equivalent table name in the DB 
+echo Path to CSV:
+read CSV_file
+echo DB table name to import into:
+read DB_table
+
+# Create .txt file that will contain SQL INSERT STATEMENT and enter DB table name
+echo "INSERT INTO $DB_table VALUES" > SQL_INSERT_$DB_table.txt
+
+# Auto-quote string columns in the .txt file, leaving other columns intact
+awk -F, 'OFS=FS {for (i=1;i<=NF;i++) {if (match($i, /^[0-9.-]+$/)==0) {printf "\"" $i "\""} else {printf $i}; if (i<NF) printf OFS}; printf "\n"}' $CSV_file > temp.txt
+
+# read-while loop to populate INSERT STATEMENT row values from CSV (2nd row to the end) and replace final comma with semicolon for those RDBMS's that require a concluding semicolon at the end of SQL STATEMENT
+while read line
+do
+ echo "($line),"
+done < <(tail -n +2 temp.txt) >> SQL_INSERT_$DB_table.txt && sed -i '' '$ s/.$/;/' SQL_INSERT_$DB_table.txt
+
+# Delete temporary .txt file that contained auto-quoted string values
+rm temp.txt
