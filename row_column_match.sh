01c3b3bd91ba4935b32437c15785fccd0d28f648
McKasra <50338667+McKasra@users.noreply.github.com>
2021-03-26 14:41:53 -0700

    Update row_column_match.sh

--- row_column_match.sh
+++ row_column_match.sh
@@ -7,11 +7,11 @@ echo Literal or regex string to find:
 read string
 
 # Define variable and test if any matches are to be found; if not, notification is sent to terminal, but if matches exist, their row numbers (as summary rows) and individual column numbers will be output to a .txt file in the home directory
-matchesFound=$(cat $file | grep -c "$string")
+matchesFound=$(cat $file | grep -E -c "$string")
 if [ $matchesFound -eq 0 ];
 then
     echo "No matches exist."
 else
-    printf "Summary Row No: \n`awk -v awkvar="$string" '$0 ~ awkvar{print NR}' $file`" > string_match_results.txt
-    printf "\nInstance Column No. \n`awk -v awkvar="$string" -F"," '{for(i=1;i<=NF;i++){if ($i ~ awkvar){print i}}}' $file`" >> string_match_results.txt
+    printf "Summary Row No: \n`awk -v awkvar="$string" '$0 ~ awkvar{print NR}' $file`" > results_for_$string.txt
+    printf "\nInstance Column No. \n`awk -v awkvar="$string" -F"," '{for(i=1;i<=NF;i++){if ($i ~ awkvar){print i}}}' $file`" >> results_for_$string.txt
 fi
